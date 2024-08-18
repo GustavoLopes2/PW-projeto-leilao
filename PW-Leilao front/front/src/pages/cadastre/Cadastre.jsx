9159e21f99e6a36f760bdc4e4df48e3e1b297f12
Gustavo Lopes Feitosa <103670918+GustavoLopes2@users.noreply.github.com>
2024-08-17 22:50:57 -0300

    Esse commit incluirá a validação de senha, com regras definidas.

--- PW-Leilao front/front/src/pages/cadastre/Cadastre.jsx
+++ PW-Leilao front/front/src/pages/cadastre/Cadastre.jsx
@@ -1,43 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import './Cadastre.css';
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
-
+import { validatePassword } from './../../validations/passwordValidation';
 
 const Cadastre = () => {
+    const [password, setPassword] = useState("");
+    const [confirmedPassword, setConfirmedPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+    const [isFormValid, setIsFormValid] = useState(false);
+
+    const handlePasswordChange = (e) => {
+        const value = e.target.value;
+        setPassword(value);
+        validatePasswords(value, confirmedPassword);
+    };
+
+    const handleConfirmedPasswordChange = (e) => {
+        const value = e.target.value;
+        setConfirmedPassword(value);
+        validatePasswords(password, value);
+    };
+
+    const validatePasswords = (password, confirmedPassword) => {
+        const passwordErrorMessage = validatePassword(password);
+        setPasswordError(passwordErrorMessage);
+
+        if (password !== confirmedPassword) {
+            setConfirmPasswordError("Passwords do not match");
+        } else {
+            setConfirmPasswordError("");
+        }
+
+        setIsFormValid(passwordErrorMessage === "" && confirmedPassword === password && confirmedPassword !== "");
+    };
+
     return (
         <div className="cadastre-container">
             <Card title="Cadastrar">
-                <div class="field grid">
-                    <label for="nome" style={{ width: 100 }} class="col-fixed">Nome</label>
-                    <div class="col">
-                        <InputText id="nome" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary" />
+                <div className="field grid">
+                    <label htmlFor="nome" style={{ width: 100 }} className="col-fixed">Nome</label>
+                    <div className="col">
+                        <InputText id="nome" type="text" className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary" />
                     </div>
                 </div>
-                <div class="field grid">
-                    <label for="login" style={{ width: 100 }} class="col-fixed">Login</label>
-                    <div class="col">
-                        <InputText id="login" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary" />
+                <div className="field grid">
+                    <label htmlFor="login" style={{ width: 100 }} className="col-fixed">Login</label>
+                    <div className="col">
+                        <InputText id="login" type="text" className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary" />
                     </div>
                 </div>
-                <div class="field grid">
-                    <label for="password" style={{ width: 100 }} class="col-fixed">Password</label>
-                    <div class="col">
-                        <Password id="password" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary" />
+                <div className="field grid">
+                    <label htmlFor="password" style={{ width: 100 }} className="col-fixed">Password</label>
+                    <div className="col">
+                        <Password
+                            id="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            className="input-field p-password"
+                            feedback={false}
+                            toggleMask
+                        />
+                        <div className="error-container">
+                            {passwordError && <small className="p-error">{passwordError}</small>}
+                        </div>
                     </div>
                 </div>
-                <div class="field grid">
-                    <label for="confirmed-password" style={{ width: 100 }} class="col-fixed">Confirmed Password</label>
-                    <div class="col">
-                        <Password id="confirmed-password" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary" />
+                <div className="field grid">
+                    <label htmlFor="confirmed-password" style={{ width: 100 }} className="col-fixed">Confirmed Password</label>
+                    <div className="col">
+                        <Password
+                            id="confirmed-password"
+                            value={confirmedPassword}
+                            onChange={handleConfirmedPasswordChange}
+                            className="input-field p-confirmed-password"
+                            feedback={false}
+                            toggleMask
+                        />
+                        <div className="error-container">
+                            {confirmPasswordError && <small className="p-error">{confirmPasswordError}</small>}
+                        </div>
                     </div>
                 </div>
-                <Button label="Cadastrar" disabled style={{ marginRight: 80 }}/>
+                <Button label="Cadastrar" style={{ marginRight: 80 }} disabled={!isFormValid} />
                 <Button label="Cancelar" onClick={() => window.open("/login")} />
             </Card>
-
         </div>
     );
 }
--- PW-Leilao front/front/src/pages/login/Login.jsx
+++ PW-Leilao front/front/src/pages/login/Login.jsx
@@ -1,32 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css';
+import './../Style.css';
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
-
+import { validatePassword } from './../../validations/passwordValidation';
 
 const Login = () => {
+    const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleLoginClick = () => {
+        const errorMessage = validatePassword(password);
+        setPasswordError(errorMessage);
+        const isValid = errorMessage === "";
+
+        if (isValid) {
+            setIsPasswordValid(true);
+            window.open("/");
+        } else {
+            setIsPasswordValid(false);
+        }
+    };
+
     return (
         <div className="login-container">
             <Card title="Login">
-                <div class="field grid">
-                    <label for="login" style={{ width: 100 }} class="col-fixed">Login</label>
-                    <div class="col">
-                        <InputText id="login" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary"/>
+                <div className="field grid">
+                    <label htmlFor="login" style={{ width: 100 }} className="col-fixed">Login</label>
+                    <div className="col">
+                        <InputText 
+                            id="login" 
+                            type="text" 
+                            className="input-field"
+                        />
                     </div>
                 </div>
-                <div class="field grid">
-                    <label for="password" style={{width: 100 }} class="col-fixed">Password</label>
-                    <div class="col">
-                        <Password id="password" type="text" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary"/>
+                <div className="field grid">
+                    <label htmlFor="password" style={{ width: 100 }} className="col-fixed">Password</label>
+                    <div className="col">
+                        <Password 
+                            id="password"
+                            value={password} 
+                            onChange={handlePasswordChange} 
+                            className="input-field p-password"
+                            feedback={false} 
+                            toggleMask 
+                        />
+                        <div className="error-container">
+                            {passwordError && <small className="p-error">{passwordError}</small>}
+                        </div>
                     </div>
                 </div>
-                <Button label="Login" disabled onClick={() => window.open("/")} style={{ marginRight: 20 }} />
+                <Button label="Login" onClick={handleLoginClick} style={{ marginRight: 20 }}/>
                 <Button label="Cadastrar" onClick={() => window.open("/cadastre")} style={{ marginRight: 20 }} />
                 <Button label="Recuperar Senha" onClick={() => window.open("/recover-password")} />
             </Card>
-
         </div>
     );
 }
